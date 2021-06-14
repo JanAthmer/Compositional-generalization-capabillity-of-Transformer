645c1b1ccd958e42857cd2a74dc63bd1c49ab6e8
Piemol <janathmer@hotmail.com>
2021-06-14 20:21:41 +0200

    Fixed the data addx, did a lot of runs and made a new notebook for these runs to make the plots.

--- TURN_X_test.py
+++ TURN_X_test.py
@@ -4,12 +4,19 @@ def main():
     with open("source.txt", "w") as f:
         for line in lines:
             f.write(line.replace("turn left", "turn x"))
-    print("completed 1/2")
+    print("completed 1/3")
+
+    with open("source.txt", "r") as f:
+        lines = f.readlines()
+    with open("source.txt", "w") as f:
+        for line in lines:
+            f.write(line.replace("left", "x"))
+    print("completed 2/3")
 
     with open("target.txt", "r") as f:
         lines = f.readlines()
     with open("target.txt", "w") as f:
         for line in lines:
             f.write(line.replace("I_TURN_LEFT", "I_TURN_X"))
-    print("completed 2/2")
+    print("completed 3/3")
 main()
